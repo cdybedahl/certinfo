85744b5798818df442867f770dcba23b66b053ff
Calle Dybedahl <github@calle.dybedahl.se>
2022-01-22 15:56:44 +0100

    Error handling.

--- src/certinfo.erl
+++ src/certinfo.erl
@@ -15,27 +15,31 @@ main([Host]) ->
 main([Host, Port0]) ->
     application:ensure_all_started(ssl),
     Port = erlang:list_to_integer(Port0),
-    {ok, SSLSocket} = ssl:connect(Host, Port, []),
-    {ok, DERCert} = ssl:peercert(SSLSocket),
-    OTPCert = public_key:pkix_decode_cert(DERCert, otp),
-    Cert = OTPCert#'OTPCertificate'.tbsCertificate,
-    Extensions = Cert#'OTPTBSCertificate'.extensions,
-    Validity = Cert#'OTPTBSCertificate'.validity,
-    NotBefore = str2datetime(Validity#'Validity'.notBefore),
-    NotAfter = str2datetime(Validity#'Validity'.notAfter),
-    io:format("Not valid before: ~s~n", [datetime2str(NotBefore)]),
-    io:format("Not valid after: ~s~n", [datetime2str(NotAfter)]),
-    SAN = lists:keyfind({2, 5, 29, 17}, 2, Extensions),
-    Names = lists:map(fun({dNSName, Name}) -> Name end, SAN#'Extension'.extnValue),
-    io:format("~nSubject names:~n"),
-    lists:foldl(
-        fun(N, Acc) ->
-            io:format(" ~3b: ~ts~n", [Acc, N]),
-            Acc + 1
-        end,
-        1,
-        Names
-    ),
+    case ssl:connect(Host, Port, [{verify, verify_none}], 10_000) of
+        {ok, SSLSocket} ->
+            {ok, DERCert} = ssl:peercert(SSLSocket),
+            OTPCert = public_key:pkix_decode_cert(DERCert, otp),
+            Cert = OTPCert#'OTPCertificate'.tbsCertificate,
+            Extensions = Cert#'OTPTBSCertificate'.extensions,
+            Validity = Cert#'OTPTBSCertificate'.validity,
+            NotBefore = str2datetime(Validity#'Validity'.notBefore),
+            NotAfter = str2datetime(Validity#'Validity'.notAfter),
+            io:format("Not valid before: ~s~n", [datetime2str(NotBefore)]),
+            io:format("Not valid after: ~s~n", [datetime2str(NotAfter)]),
+            SAN = lists:keyfind({2, 5, 29, 17}, 2, Extensions),
+            Names = lists:map(fun({dNSName, Name}) -> Name end, SAN#'Extension'.extnValue),
+            io:format("~nSubject names:~n"),
+            lists:foldl(
+                fun(N, Acc) ->
+                    io:format(" ~3b: ~ts~n", [Acc, N]),
+                    Acc + 1
+                end,
+                1,
+                Names
+            );
+        {error, Err} ->
+            io:format("Connection failed: ~p~n", [Err])
+    end,
     erlang:halt(0).
 
 %%====================================================================
@@ -59,7 +63,9 @@ str2datetime({utcTime, S}) ->
         }
     }.
 
-datetime2str({{Year, Month, Day}, {Hour, Minute, Second}}) ->
-    io_lib:format("~b-~2..0b-~2..0b ~2..0b:~2..0b:~2..0b UTC", [
+datetime2str(UT) ->
+    LT = calendar:universal_time_to_local_time(UT),
+    {{Year, Month, Day}, {Hour, Minute, Second}} = LT,
+    io_lib:format("~b-~2..0b-~2..0b ~2..0b:~2..0b:~2..0b (local time)", [
         Year, Month, Day, Hour, Minute, Second
     ]).
